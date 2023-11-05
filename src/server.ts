3e0164fbffe1ef12a75d2819f98446797c138587
alpha951 <20uec068@lnmiit.ac.in>
2023-11-06 02:06:07 +0530

    Add config setup

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-function login(username: string, password: string) {
-  if (username == "admin" && password == "admin") {
-    return true;
-  } else {
-    return false;
-  }
-}
-login("admin", "admin");
